a5ea973b9de1b09711caf13567812295d415a8ab
Cristian Sombra <cristiansombra87@gmail.com>
2023-12-25 20:13:11 -0300

    Actualizción de estilo.
    Se actualiza estilo y posicionamiento.

--- aguilasdelsur/src/views/landing/landing.jsx
+++ aguilasdelsur/src/views/landing/landing.jsx
@@ -1,41 +1,37 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/landing.png"
+import logo from "../../assets/landing.png";
 import "./landing.css";
 
 const Landing = () => {
     useEffect(() => {
-        // Ocultar barras de desplazamiento al cargar la página
-        document.body.style.overflow = 'hidden';
-    
-        // Restaurar barras de desplazamiento al desmontar el componente
-        return () => {
-        document.body.style.overflow = 'visible';
-        };
-    }, []);
+    // Ocultar barras de desplazamiento al cargar la página
+    document.body.style.overflow = "hidden";
 
+    // Restaurar barras de desplazamiento al desmontar el componente
+    return () => {
+        document.body.style.overflow = "visible";
+    };
+}, []);
 
-    return (
-        <div className="container-fluid container-fluid-landing">
-            <div className="container-intro " style={{ minHeight: '100vh' }}>
-                <div className="row d-flex align-items-center justify-content-center">
-                    <div className="col-12 text-center">
-                        <img src={logo} className="img-fluid mt-3" alt="fondo" />
-                    </div>
-                    <div className="col-12 text-center fire-text">
-                        <h3>Agrupación Motera</h3>
-                        <h3>Águilas del Sur</h3>
-                    </div>
-                    <div className="col-12 text-center">    
-                        <Link to="/home" className="my-3 icon-landing">
-                            <i class="bi bi-box-arrow-in-right"></i>
-                        </Link>
-                    </div>
-                </div>
+return (
+    <div className="container-fluid container-fluid-landing d-flex align-items-center justify-content-center">
+        <div className="container-intro text-center">
+            <div>
+                <img src={logo} className="img-fluid mt-3 img-landing" alt="fondo" />
+            </div>
+            <div className="fire-text">
+                <h2>Agrupación Motera</h2>
+                <h3>Águilas del Sur</h3>
+            </div>
+            <div>
+                <Link to="/home" className="my-3 icon-landing">
+                    <i className="bi bi-box-arrow-in-right"></i>
+                </Link>
             </div>
         </div>
+    </div>
     );
 };
-
 
 export default Landing;
