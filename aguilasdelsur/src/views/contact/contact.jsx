1db44267335d01aa57b9d849e2dbd7975ab61236
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-01-06 14:23:10 -0300

    Contact, Footer, Scrolltop
    - Contact: Se crea formulario.
    - Footer: Se corrige autor.
    - Scrolltop: Se agrega animación.

--- aguilasdelsur/src/views/contact/contact.jsx
+++ aguilasdelsur/src/views/contact/contact.jsx
@@ -1,86 +1,45 @@
 import React from "react";
-import ScrollTop from "../../components/scrollTop/scrollTop"
+import ScrollTop from "../../components/scrollTop/scrollTop";
+import imgcontact from "../../assets/contact.png";
+import "../index.css";
 
 function Contact () {
     return (
-        <div className="container-fluid">
-                <div className="row mt-5 d-flex justify-content-center">
-                    <div className="col-12 text-center">
-                        <h1>La vista de contacto está en construcción</h1>    
+        <div className="container">
+                <div className="row my-5 m-3 d-flex justify-content-around align-items-center">
+                    <div className="col-12 col-md-6 my-3 text-center">
+                        <p>Águilas del Sur se caracteriza por su camadería, recordando el título de la célebre película de <strong>Terence Hill & Bud Spencer</strong>, "quien tiene un amigo motero...¡Tiene un Tesoro!</p>
+                        <p>Dicho esto si quieres formar parte de la agrupación o si no eres por ésta zona puedes contar con nosotros completando el formulario!</p>    
+                        <h1>¡Contáctanos!</h1>
+                        <img 
+                            src={imgcontact} 
+                            className="img-fluid rounded my-3"
+                            style={{boxShadow: '0px 0px 10px 2px rgba(0, 0, 0, 0.3)' }}    
+                        />
+                    </div>
+                    <div className="col-12 col-md-4 my-3 mi-formulario p-3">
+                        <form action="">
+                            <div className="mb-3">
+                                <label htmlFor="nombre y apellido" className="form-label">Nombre y Apellido</label>
+                                <input type="text" className="form-control" id="nombre y apellido" aria-describedby="ayuda-nombra"/>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="correo" className="form-label">Correo electrónico</label>
+                                <input type="text" className="form-control" id="correo" aria-describedby="ayuda-correo"/>
+                                <div id="ayuda-correo" className="form-text text-white">
+                                    Te enviaremos un correo para confirmar
+                                </div>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="mensaje" className="form-label">Mensaje</label>
+                                <textarea name="mensaje" id="mensaje" className="form-control"></textarea>
+                            </div>
+                            <div className="d-flex justify-content-end">
+                                <button type="submit" className="btn btn-outline-light">Enviar</button>
+                            </div>
+                        </form> 
                     </div>    
-                </div>
-            <div className="row justify-content-center">
-                <div className="col-12 text-center">
-                    <div className="row mt-5 justify-content-center">
-                        <div className="col-md-3">
-                            <p>En construcción</p>
-                        </div>
-                    </div>
-                    <div className="row my-5 justify-content-around">
-                        <div className="col-3">
-                            <p>En construcción</p>                         
-                        </div>
-                        <div className="col-3">
-                            <p>En construcción</p>                        
-                        </div>
-                        <div className="col-3">
-                            <p>En construcción</p>                        
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="row my-5">
-                        <div className="col-12 mb-3">
-                            <p>En construcción</p>                         
-                        </div>
-                        <div className="col-12 mb-3">
-                            <p>En construcción</p>                         
-                        </div>
-                        <div className="col-12 mb-3">
-                            <p>En construcción</p>                         
-                        </div>
-                        </div>
-                    <hr />
-                    <div className="row my-5">
-                        <div className="col mb-3 d-flex justify-content-end">
-                            <p>En construcción</p>                     
-                        </div>
-                    </div>
-                    <div className="row my-5">
-                        <div className="col-12 mb-3">
-                            <p>En construcción</p>                     
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="row my-5">
-                        <div className="col">
-                            <p>En construcción</p>                     
-                        </div>
-                        <div className="col">
-                            <p>En construcción</p>                     
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="row my-5">
-                        <div className="col">
-                            <p>En construcción</p>
-                        </div>
-                    </div>
-                    <div className="row my-5">
-                        <div className="col">
-                            <p>En construcción</p>
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="row my-5 justify-content-evenly">
-                        <div className="col-3">
-                            <p>En construcción</p>
-                        </div>
-                        <div className="col-3">
-                            <p>En construcción</p>
-                        </div>
-                    </div>
-                </div>
-            </div>    
+                </div> 
         <ScrollTop/>
         </div>
     )
